9b5ab33e77b12a4156252722f5abd4f179dd3dd9
CatherineLuquire <cluquire@gmail.com>
2020-09-09 17:27:24 -0400

    final code finished, need to style

--- app.py
+++ app.py
@@ -4,26 +4,19 @@ import scrape_mars
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-# mongo = PyMongo(app)
-
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-
 @app.route("/")
-def index():
-    mars_info = mongo.db.mars_collection.find_one()
-    return render_template("index.html", mars_info=mars_info)
-
+def home():
+    mars_info = mongo.db.mars.find_one()
+    return render_template("index.html", marsdata=mars_info)
 
 @app.route("/scrape")
 def scrape():
-    # mars = mongo.db.mars
-    data = scrape_mars.scrape()
-    mongo.db.mars_collection.update({}, data, upsert=True)
-    return redirect("/", code=302)
-
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping successful!"
 
 if __name__ == "__main__":
     app.run(debug=True)
